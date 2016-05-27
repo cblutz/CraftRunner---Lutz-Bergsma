943662b0c9e568851f6ac7790065b542212578cd
cblutz <cblutz@guerincatholic.org>
2016-05-27 08:32:48 -0400

    First Commit

---
+++ CraftRunnerTests/CraftRunnerTests.swift
@@ -0,0 +1,36 @@
+//
+//  CraftRunnerTests.swift
+//  CraftRunnerTests
+//
+//  Created by Casey Lutz on 5/27/16.
+//  Copyright © 2016 Lutz Bergsma Software. All rights reserved.
+//
+
+import XCTest
+@testable import CraftRunner
+
+class CraftRunnerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
